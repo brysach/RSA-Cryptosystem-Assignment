5e6696798297f7abe8d88e7b777e539efae68e71
brysach <bryan.estradach@gmail.com>
2025-10-20 00:35:24 -0700

    Added function to find p and q when P(e,n) is valid

--- main.cpp
+++ main.cpp
@@ -21,22 +21,43 @@ int main(){
         cin >> cyphertext[i];
     }
 
+    //cout << isPrime(5);
+
     if(!pIsValid(n, p, q))
         cout << "Public key is not valid!";
+    
+    cout << p << "-" << q;
 
     return 0;
 }
 
 bool isPrime(int n){
     if(n < 2) return false;
-    if(n == 2 || n == 3) return true;
-    double val = static_cast<double>(n);    
+    if(n == 2) return true;
+
+    double val = static_cast<double>(n);   
+
     for(int i = 2; i <= static_cast<int>(floor(sqrt(val))); i++){
         if(n % i == 0) return false;
-        else return true;
     }
+
+    return true;
 }
 
 bool pIsValid(int n, int& p, int& q){
+    bool pFound = false;
     if(isPrime(n)) return false;
+    for(int i = 2; i <= n/2; i++){
+        if(n % i == 0 && isPrime(i) && !pFound){
+            p = i;
+            pFound = true;
+            continue;
+        }
+        if(n % i == 0 && isPrime(i)){
+            q = i;
+            break;
+        }
+    }
+
+    return true;
 }
